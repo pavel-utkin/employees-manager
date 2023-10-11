c23298fb7687963124d87fa55f0986a62a1b651b
Pavel Utkin <pavel.utkin@abbyy.com>
2023-10-12 00:03:04 +0300

    parse configs

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module employees-manager
 go 1.19
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.10 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "github.com/sirupsen/logrus"
+import (
+	"flag"
+	"github.com/caarlos0/env/v6"
+	"github.com/sirupsen/logrus"
+)
 
 type Config struct {
 	AddressREST string `env:"AddressREST"`
@@ -12,5 +16,13 @@ func NewConfig(log *logrus.Logger) *Config {
 		AddressREST: "localhost:8088",
 		DSN:         "root:password@tcp(127.0.0.1:3306)/Employees",
 	}
+	flag.StringVar(&configServer.AddressREST, "a", configServer.AddressREST, "Server address")
+	flag.StringVar(&configServer.DSN, "d", configServer.DSN, "DATABASE DSN")
+	flag.Parse()
+	err := env.Parse(&configServer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Debug(configServer)
 	return &configServer
 }
